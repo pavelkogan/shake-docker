b4406f75aad19a3636687a05aa26e06b6e5bae0f
Pavel Kogan <pkogan@gmail.com>
2015-03-25 15:15:05 +0000

    Extensive changes

--- shake-docker.hs
+++ shake-docker.hs
@@ -2,17 +2,19 @@
 
 {-# LANGUAGE NoImplicitPrelude #-}
 
+module Main where
+
 import BasePrelude
-import Data.Version
+import Data.Version ()
 import Development.Shake
 import Development.Shake.FilePath
 import Safe
 import Text.Printf
 
 data DockerImage = DockerImage
-    { name :: String
-    , tag  :: String
-    , id_  :: String
+    { _name :: String
+    , _tag  :: String
+    , _id   :: String
     } deriving (Show, Read, Eq)
 
 dockerImage :: String -> DockerImage
@@ -42,12 +44,14 @@ data Project = Project
     , gitRevision :: String
     } deriving (Eq, Show)
 
+getProject :: Action Project
 getProject = do
     [f] <- getDirectoryFiles "" ["*.cabal"]
     let n = takeBaseName f
     r <- getGitRevision
     return $ Project n f r
 
+getGitRevision :: Action String
 getGitRevision = do
     Stdout r <- cmd "git rev-parse --short HEAD"
     return $ head $ words r  -- drops newline
@@ -71,46 +75,68 @@ main = shakeArgs shakeOptions{shakeFiles="_shake/"} $ do
     "_shake/*.lts-cabal.image" %> \ out -> do
         let config = dropDirectory1 $ out -<.> "config"
         let v = dropExtension2 config
-        need [config]
-        withTempDir $ \ buildDir -> do
-            writeFile' (buildDir </> "Dockerfile") ltsDockerfile
-            copyFile' config $ buildDir </> "lts-cabal.config"
-            let v' = "7.8-" <> v
-            () <- cmd (Cwd buildDir) $ "docker build -t haskell-lts:"<>v'<>" ."
-            imageId <- getImageId "haskell-lts" v'
-            writeFileChanged out imageId
+        ltsDockerfile `withClean` [(config, "lts-cabal.config")] $ \ buildDir ->
+            dockerBuild (Just buildDir) Nothing "haskell-lts" ("7.8-" <> v) out
+    "_shake/*.cabal" %> \ out ->
+        copyFileChanged (dropDirectory1 out) out
+    "_shake/*.project-deps.image" %> \ out -> do
+        Project n _ _ <- getProject
+        d <- readFileLines "Dockerfile"
+        let c = n <.> ".cabal"
+            f = [("_shake" </> c, c)]
+        dockerInstallDeps n d `withClean` f $ \ buildDir ->
+            dockerBuild (Just buildDir) Nothing (n <> "-deps") "latest" out
     "_shake/*.project.image" %> \ out -> do
+    -- TODO: create dockerfile with FROM being deps image
         Project n _ r <- getProject
         let revision = dropDirectory1 $ dropExtension2 out
         when (r /= revision) $ error "git revision error"
-        () <- cmd (printf "docker build -t %s:%s ." n r :: String)
-        imageId <- getImageId n r
-        writeFileChanged out imageId
+        dockerBuild Nothing Nothing n r out
     "project" ~> do
         Project n _ r <- getProject
         need [printf "_shake/%s.project.image" r]
         cmd (printf "docker tag -f %s:%s %s:%s" n r n "latest" :: String)
     "lts-versions" ~> do
         images <- getImages "haskell-lts"
-        liftIO $ mapM_ (putStrLn . tag) images
+        liftIO $ mapM_ (putStrLn . _tag) images
+
+withClean :: String                  -- ^ Dockerfile contents
+          -> [(FilePath, FilePath)]  -- ^ map files to relative path in buildDir
+          -> (FilePath -> Action a)  -- ^ takes buildDir
+          -> Action a
+withClean d fs a = withTempDir $ \ b -> do
+    writeFile' (b </> "Dockerfile") d
+    forM_ fs $ \(x, y) -> copyFile' x (b </> y)
+    a b
+
+dockerBuild :: Maybe FilePath  -- ^ build directory
+            -> Maybe FilePath  -- ^ optional Dockerfile
+            -> String          -- ^ image name
+            -> String          -- ^ image tag
+            -> FilePath        -- ^ file to save image id to
+            -> Action ()
+dockerBuild buildDir dockerfile name tag imageIdFile = do
+    let cmd' = maybe cmd (cmd . Cwd) buildDir
+        df   = maybe "" ("-f " <>) dockerfile
+    () <- cmd' (printf "docker build %s -t %s:%s ." df name tag :: String)
+    imageId <- getImageId name tag
+    writeFileChanged imageIdFile imageId
 
 getImageId :: String -> String -> Action String
-getImageId n t = do
-    images <- getImages n
-    return $ id_ $ head $ filter ((t ==) . tag) images
+getImageId t = return . _id . head . filter ((t ==) . _tag) <=< getImages
 
 getImages :: String -> Action [DockerImage]
 getImages s = do
     Stdout out <- cmd $ "docker images " <> s
     return $ dockerImages out
 
-projectDockerfile n = unlines
-    [ "FROM haskell-lts:latest"
-    , printf "ADD ./ /%s/" n
-    , printf "RUN cd /%s && cabal install" n
-    , printf "WORKDIR /%s" n
+dockerInstallDeps :: String -> [String] -> String
+dockerInstallDeps n d = unlines $ d ++
+    [ printf "ADD ./ /%s/" n
+    , printf "RUN cd /%s && cabal install --dependencies-only" n
     ]
 
+ltsDockerfile :: String
 ltsDockerfile = unlines
     [ "FROM haskell:7.8"
     , "RUN cabal update"
@@ -120,10 +146,14 @@ ltsDockerfile = unlines
     , "ENV PATH /root/.cabal/bin:$PATH"
     ]
 
+dockerImages :: String -> [DockerImage]
 dockerImages = map dockerImage . drop 1 . lines
 
+dropExtension2 :: FilePath -> FilePath
 dropExtension2 = dropExtension . dropExtension
 
+curlConfig :: FilePath -> FilePath
 curlConfig v = "curl http://www.stackage.org/lts" </> v </> "cabal.config?global=true"
 
+getLtsVersion :: String -> String
 getLtsVersion = dropWhile (not . isDigit) . head . lines
